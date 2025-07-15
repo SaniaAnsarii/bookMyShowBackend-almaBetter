5387ad8c6bf7ddd2c5fedd98d8811fe83b5d5cf7
SaniaAnsarii <ansarisania789@gmail.com>
2025-07-16 01:47:43 +0530

    Add backend source code

---
+++ db/connection.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require("mongodb");
+let mongoose = require("mongoose");
+require("dotenv").config();
+
+const mongoLiveURI = process.env.MONGO_LIVE_URL;
+const mongoLocalURI = process.env.MONGO_URL || "mongodb://localhost:27017/bookMyShow"
+const mongoURI = mongoLiveURI || mongoLocalURI;
+
+const connectToMongo = async () => {
+  // Connecting to database using connection string and speciying if there is any error or it was successfull
+  mongoose
+    .connect( mongoURI, 
+        { 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true 
+        }
+    )
+    .then(() => {
+      console.log("Database connected");
+    })
+    .catch((err) => {
+      console.log("Database connection error", err);
+    });
+};
+
+exports.connection = connectToMongo;
